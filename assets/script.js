cb5cc82617a1d5648c13fa947bfab0ead248f63b
canna88 <97223496+canna88@users.noreply.github.com>
2023-07-19 20:08:45 +0200

    Script - prova aggiunta

--- assets/script.js
+++ assets/script.js
@@ -97,3 +97,109 @@
         incorrect_answers: ["Python", "C", "Jakarta"],
       },
     ];
+
+    
+// Caricamento della pagina
+window.onload = function() {
+
+  // Identificare gli elementi nel DOM
+  const buttonProceed = document.getElementById("button")
+  buttonProceed.disabled = true;
+
+  // Clicca o no
+  checkBox.addEventListener("change", function () {
+      if (checkBox.checked === true) {
+          buttonProceed.disabled = false;
+      } else {
+          buttonProceed.disabled = true;
+      }
+  })
+
+
+  // FUNZIONE CREAZIONE ARRAY CON TUTTE LE RISPOSTE
+  function allAnswers (object) {
+      let array = object.incorrect_answers
+      array.push(object.correct_answer)
+      return array
+  }
+  
+  // FUNZIONE CREAZIONE ARRAY DOMANDE CASUALI
+  function mescola(arrayMain) {
+      let array = arrayMain.slice()
+      let currentIndex = array.length, temporaryValue, randomIndex; // Ci prendiamo la lunghezza dell'array e partiamo dal fondo!
+      // Finché ci sono elementi da mescolare, iteriamo l'array
+      while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex); // Prendiamo un indice a caso dell'array, purché sia compreso tra 0 e la lunghezza dell'array
+          currentIndex -= 1; // Riduciamo di un'unità l'indice corrente
+          
+          // Una volta che abbiamo preso l'indice casuale, invertiamo l'elemento che stiamo analizzando alla posizione corrente (currentIndex) con quello alla posizione presa casualmente (randomIndex)
+          temporaryValue = array[currentIndex];     // Variabile temporanea
+          array[currentIndex] = array[randomIndex];    // Eseguiamo lo scambio
+          array[randomIndex] = temporaryValue;
+      }
+      return array    
+  }
+
+  // Identificare gli elementi nel DOM
+  const title = document.getElementById("title");
+  const divButtons = document.getElementById("divButtons");
+  const buttonCheck = document.getElementById("buttonCheck")
+  const divCheck = document.getElementById("divResult")
+
+  // Creo le variabili
+  let punteggio = 0
+  let numDomande = questions.length
+
+  let currentQuestionIndex = 0
+
+  function showQuestions () {
+
+      // Assegno il testo della domanda
+      title.innerText = questions[currentQuestionIndex].question
+      
+      // Prendo tutte le domande e le mescolo
+      let vettore = []
+      let vettore2 = []
+      vettore = allAnswers(questions[currentQuestionIndex])
+      vettore2 = mescola(vettore)
+      divButtons.innerHTML = ""
+
+      // Ciclo per creare i pulsanti
+      for (let i = 0; i < vettore2.length; i++) {
+          
+          let input = document.createElement("input");
+          input.value = vettore2[i];
+          input.type = "radio";
+          input.name = "answer";
+          
+          let label = document.createElement("label");
+          label.innerText = vettore2[i];
+
+          divButtons.appendChild(input);
+          divButtons.appendChild(label);
+      }
+
+      // Passa alla domanda successiva
+      currentQuestionIndex += 1
+  }
+  
+  function checkAnswers () {
+      let selectedAnswer = document.querySelector("input[type=radio]:checked");
+  
+      if (selectedAnswer) {
+          let selectedValue = selectedAnswer.value;
+  
+          if (selectedValue === questions[currentQuestionIndex].correct_answer) {
+              divCheck.innerText = "Correct";
+              punteggio += 1
+          } else {
+              divCheck.innerText = "Incorrect";
+          }
+      } else {
+          divCheck.innerText = "Please select an answer.";
+      }
+  };
+
+  showQuestions()
+  setInterval(checkAnswers, 100)
+}
